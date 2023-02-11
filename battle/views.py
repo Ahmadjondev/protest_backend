f4ac77561bb2fe5d4ab3890d7134477c0f82455c
Ahmadjon <ahmadjondevv@gmail.com>
2023-02-11 20:41:13 +0500

    ProTest

--- battle/views.py
+++ battle/views.py
@@ -1,5 +1,3 @@
-import random
-
 from django.forms import model_to_dict
 from rest_framework.response import Response
 from rest_framework.views import APIView
--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('api/v1/', include('quiz.urls')),
     path('api/v1/', include('battle.urls')),
     path('api/v1/', include('universities.urls')),
-]
-# +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
